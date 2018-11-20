81bdd7b63b7035bf65e8bfb58e3ed463c761beeb
Laszlo Katai-Pal <lkpal@dir.eeft.com>
2018-11-20 18:55:54 +0100

    started interview questions and answers

--- hello.py
+++ hello.py
@@ -1,7 +1,23 @@
-x = 9 + 6
-y = 1 + 1
-print (x)
-print (y)
-print ("hello world")
-print ("adj")
-print (3)
+## 50 interview questions and answers
+
+## What is the difference between list and tuples?
+
+##          Lists
+##              lists can be edited
+##              lists are slower than tuples
+list_1 = [10, 'Chelsea', 20]
+
+##          Tuples
+##              Tuples cannot be edited
+tup_1 = (10, 'Chelsea', 20)
+
+## How can the ternary operators be used in python?
+
+## The Ternary operator is the operator that is 
+##  used to show the conditional statements. 
+## This consists of the true or false values with a statement that has to be evaluated for it.
+
+x = 50
+y = 20
+big = x if x>50 else y 
+print ("Big is: " +  str(big))
